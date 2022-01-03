7fd87b34af28767bc3932a1b9a18460efd3df492
Vikas Kumar <vsrathod39@gmail.com>
2022-01-03 15:30:48 +0530

    form in progress

--- recipe-tracking/src/components/RecipeForm.Component.jsx
+++ recipe-tracking/src/components/RecipeForm.Component.jsx
@@ -1,9 +1,48 @@
+import { useRef, useState } from "react";
 import { Div } from "../styled-components/RecipeForm.Styled";
 
 export const RecipeForm = () => {
+  const [formData, setFormData] = useState();
+
+  const handleChange = (e) => {
+    const [name, value] = e.target;
+    setFormData([{ ...formData, [name]: value }]);
+  };
+  console.log(formData);
   return (
     <>
-      <Div></Div>
+      <Div>
+        <form>
+          <input
+            onChange={handleChange}
+            type="text"
+            name="title"
+            placeholder="title"
+          />
+          <input
+            onChange={handleChange}
+            type="text"
+            name="ingredients"
+            placeholder="ingredients"
+          />
+          <input
+            onChange={handleChange}
+            type="text"
+            name="time"
+            placeholder="time to cook"
+          />
+          <input
+            onChange={handleChange}
+            type="text"
+            name="instructions"
+            placeholder="instructions"
+          />
+          <div>
+            <p>Upload Image: </p>
+            <input type="file" name="image" />
+          </div>
+        </form>
+      </Div>
     </>
   );
 };
--- recipe-tracking/src/styled-components/RecipeForm.Styled.jsx
+++ recipe-tracking/src/styled-components/RecipeForm.Styled.jsx
@@ -1,4 +1,25 @@
 import styled from "styled-components";
 export const Div = styled.div`
   border: 1px solid blue;
+  width: 30vw;
+  & form {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    & input {
+      width: 100%;
+      height: 40px;
+      text-align: center;
+      font-size: 16px;
+      outline: none;
+    }
+    & div {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      & input {
+        width: 60%;
+      }
+    }
+  }
 `;
